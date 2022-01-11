ca29c8d1049e678e0909e09a9a8752ae13a4bf9a
Ahmed Afridee <a.afridee47@gmail.com>
2022-01-11 12:28:22 +0600

    first commit

---
+++ Recursions/sumOfDigits.py
@@ -0,0 +1,12 @@
+# sum of a num's digits :
+    
+def sumofDigits(n):
+    assert n>=0 and int(n) == n , 'The number has to be a positive integer only!'
+    if n==0:
+        return 0
+    else:
+        return int(n%10) + sumofDigits(int(n/10))
+    
+print(sumofDigits(54))    
+
+
